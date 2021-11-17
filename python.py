b8ad411049bee2612e1a63354ec15e0ff335bfd9
FORCHA <forchapearl1@gmail.com>
2021-11-17 06:58:15 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,8 +1,12 @@
+import argparse
+import os
+import sys
+
 import numy as np
 from keras.applications import vgg16
 from keras.preprocessing import image
 model = vgg16.VGG16(Weights="imagenet")
-
+sys.argv[0]
 img2 = image.load_img('fly.jpeg', target_size=(224,224))
 arr2=np.expand_dims(arr2,axis=0)
 arr2 = image.img_toarray(img2)
