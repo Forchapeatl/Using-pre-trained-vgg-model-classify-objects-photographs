73734469e000843a550f36c490e091fb7e698d1a
FORCHA <forchapearl1@gmail.com>
2021-11-17 14:37:45 +0100

    Update python.py

--- python.py
+++ python.py
@@ -2,14 +2,14 @@ import argparse
 import os
 import sys
 
-import numy as np
+import numpy as np
 from keras.applications import vgg16
 from keras.preprocessing import image
-model = vgg16.VGG16(Weights="imagenet")
+model = vgg16.VGG16(weights="imagenet")
 sys.argv[0]
 img2 = image.load_img('fly.jpeg', target_size=(224,224))
-arr2=np.expand_dims(arr2,axis=0)
-arr2 = image.img_toarray(img2)
+arr2 = image.img_to_array(img2)
+arr2 = np.expand_dims(arr2,axis=0)
 arr2 = vgg16.preprocess_input(arr2)
 preds2 = model.predict(arr2)
-vgg16.decode_predictions(preds2,top=5) > somefile.txt  
+vgg16.decode_predictions(preds2,top=5) 
